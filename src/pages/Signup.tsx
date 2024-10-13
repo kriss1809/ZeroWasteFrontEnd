ddf9a4eb4351e01d58aabc369477884b990431f7
vastirrr <vasti.rus@stud.ubbcluj.ro>
2024-10-13 20:52:16 +0300

    test

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonLabel,
+  IonInput
+} from "@ionic/react";
+
+
+const Signup: React.FC = () => {
+
+    return (
+        <IonPage>
+        
+        </IonPage>
+    );
+};
+export default Signup;
