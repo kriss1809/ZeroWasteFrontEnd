c5f7d5525b1067673f52affe173a5351fbfdeb8e
kriss1809 <kristiana.gabriela18@gmail.com>
2024-10-28 10:55:17 +0200

    Refactor AddItem component to include editing functionality

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -22,10 +22,19 @@ import { useTheme } from "./ThemeContext";
 
 interface ItemProps {
   title: string;
-  expiration_date: string;
+  expiration_date: string; // Format: "DD.MM.YYYY"
+  opening_date: string;
+  recommended_days: string;
+  onEdit: (
+    title: string,
+    expiration_date: string,
+    opening_date: string,
+    recommended_days: string
+  ) => void; // Callback prop for editing
 }
 
-const ItemCard: React.FC<ItemProps> = ({ title, expiration_date }) => {
+
+const ItemCard: React.FC<ItemProps> = ({ title, expiration_date, opening_date, recommended_days, onEdit }) => {
   const history = useHistory();
   const { darkMode } = useTheme();
   const [presentAlert] = useIonAlert();
@@ -38,9 +47,30 @@ const ItemCard: React.FC<ItemProps> = ({ title, expiration_date }) => {
     console.log("Item deleted");
   };
 
+   const isExpired = () => {
+     const today = new Date();
+     const parts = expiration_date.split(".");
+     if (parts.length === 3) {
+       const formattedDate = `${parts[2]}-${parts[1]}-${parts[0]}`; // "YYYY-MM-DD"
+       const expDate = new Date(formattedDate);
+       today.setHours(0, 0, 0, 0);
+       expDate.setHours(0, 0, 0, 0);
+       return expDate < today;
+     }
+     return false;
+   };
+
+   const handleItemEdit = () => {
+    onEdit(title, expiration_date, opening_date, recommended_days); // Call the edit handler
+  };
+
+
   return (
     <div className={darkMode ? "dark-mode" : ""}>
-      <IonCard className="item-card">
+      <IonCard
+        className={`item-card ${isExpired() ? "expired" : ""}`}
+        onClick={handleItemEdit}
+      >
         <IonCardHeader>
           <IonLabel className="item-card-title">{title}</IonLabel>
         </IonCardHeader>
