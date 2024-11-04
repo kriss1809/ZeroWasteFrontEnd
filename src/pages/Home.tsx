522479c92ddda7fc462f7873f05c704ea8d1ea12
kriss1809 <kristiana.gabriela18@gmail.com>
2024-11-04 13:57:28 +0200

    Refactor Home component to add modals for uploading receipts and viewing collaborators

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 // Home.tsx
 import React, { useEffect, useState } from "react";
-import { IonHeader, IonPage, IonContent, IonButton, IonLoading, IonIcon, IonTabBar, IonTabButton, IonLabel } from "@ionic/react";
+import { IonHeader, IonPage, IonContent, IonButton, IonLoading, IonIcon, IonModal, IonList, IonItem, IonLabel, IonListHeader } from "@ionic/react";
 import Menu from "../components/Menu";
 import AddItem from "../components/AddItem";
 import ItemCard from "../components/ItemCard";
 import { useTheme } from "../components/ThemeContext";
 import { GetProductList } from "../services/apiClient";
-import { peopleOutline, receiptOutline } from "ionicons/icons";
+import { peopleOutline, receiptOutline, closeOutline } from "ionicons/icons";
 
 const Home: React.FC = () => {
   const { darkMode } = useTheme();
@@ -25,7 +25,10 @@ const Home: React.FC = () => {
     opened: string;
     consumption_days: string;
   }>>([]);
+
   const [loading, setLoading] = useState(false);
+  const [showCollaboratorsModal, setshowCollaboratorsModal] = useState(false);
+  const [showUploadModal, setshowUploadModal] = useState(false);
   
   useEffect(() => {
     const fetchData = async () => {
@@ -62,7 +65,7 @@ const Home: React.FC = () => {
           fontSize: "8vw",
         }}
       >
-        <div>
+        <div onClick={() => setshowUploadModal(true)}>
           <IonIcon icon={receiptOutline} style={{ marginRight: "3vw" }} />
         </div>
         <div
@@ -76,10 +79,131 @@ const Home: React.FC = () => {
         >
           ZeroWaste
         </div>
-        <div>
+        <div onClick={() => setshowCollaboratorsModal(true)}>
           <IonIcon icon={peopleOutline} />
         </div>
       </IonHeader>
+
+      <IonModal
+        isOpen={showUploadModal}
+        onDidDismiss={() => setshowUploadModal(false)}
+        style={{
+          maxHeight: "50vh",
+          width: "80vw",
+          margin: "auto",
+          justifyContent: "center",
+          alignitems: "center",
+          border: `1px solid ${darkMode ? "white" : "black"}`,
+        }}
+      >
+        <IonHeader
+          className="label-dark-mode"
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            borderBottom: `1px solid ${darkMode ? "white" : "black"}`,
+            position: "sticky",
+            top: 0,
+            zIndex: 10,
+            backgroundColor: "inherit",
+            padding: "0.5rem",
+          }}
+        >
+          <div style={{ fontSize: "1rem", flexGrow: 1 }}>Upload your receipt</div>
+          <div>
+            <IonIcon
+              icon={closeOutline}
+              onClick={() => setshowUploadModal(false)}
+              style={{
+                fontSize: "2rem",
+                marginLeft: "auto",
+              }}
+            />
+          </div>
+        </IonHeader>
+        <IonContent></IonContent>
+      </IonModal>
+
+      <IonModal
+        isOpen={showCollaboratorsModal}
+        onDidDismiss={() => setshowCollaboratorsModal(false)}
+        style={{
+          maxHeight: "50vh",
+          width: "80vw",
+          margin: "auto",
+          justifyContent: "center",
+          alignitems: "center",
+          border: `1px solid ${darkMode ? "white" : "black"}`,
+        }}
+      >
+        <IonContent>
+          <IonList>
+            <IonListHeader
+              className="label-dark-mode"
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                borderBottom: `1px solid ${darkMode ? "white" : "black"}`,
+                position: "sticky",
+                top: 0,
+                zIndex: 10,
+                backgroundColor: "inherit",
+              }}
+            >
+              <div style={{ fontSize: "1rem", flexGrow: 1 }}>Collaborators</div>
+              <div>
+                <IonIcon
+                  icon={closeOutline}
+                  onClick={() => setshowCollaboratorsModal(false)}
+                  style={{
+                    fontSize: "2rem",
+                    marginLeft: "auto",
+                    paddingRight: "1rem",
+                  }}
+                />
+              </div>
+            </IonListHeader>
+            <IonItem>
+              <IonLabel className="label-dark-mode">Item 1</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel className="label-dark-mode">Item 2</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel className="label-dark-mode">Item 3</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel className="label-dark-mode">Item 3</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel className="label-dark-mode">Item 3</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel className="label-dark-mode">Item 3</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel className="label-dark-mode">Item 3</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel className="label-dark-mode">Item 3</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel className="label-dark-mode">Item 3</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel className="label-dark-mode">Item 3</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel className="label-dark-mode">Item 3</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel className="label-dark-mode">Item 10</IonLabel>
+            </IonItem>
+          </IonList>
+        </IonContent>
+      </IonModal>
 
       <IonLoading
         isOpen={loading}
